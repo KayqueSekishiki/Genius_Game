a0b7db5c4e46242b1325773c90893781129b5aed
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-26 20:59:35 -0300

    piscando as cores!

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const green = document.querySelector(".green");
 const red = document.querySelector(".red");
 const yellow = document.querySelector(".yellow");
 const blue = document.querySelector(".blue");
+
+let shuffleOrder = () => {
+  let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  clickedOrder = [];
+
+  for (let i in order) {
+    let elementcolor = createColorElement(order[i]);
+    lightColor(elementcolor, Number(i) + 1);
+  }
+};
+
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add("selected");
+  }, number - 250);
+
+  setTimeout(() => {
+    element.classList.remove("selected");
+  }, number - 250);
+};
