37c178c73e9da875b440021cc32138b321f053b6
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-26 20:52:44 -0300

    reconhecendo cores

---
+++ script.js
@@ -0,0 +1,8 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+const green = document.querySelector(".green");
+const red = document.querySelector(".red");
+const yellow = document.querySelector(".yellow");
+const blue = document.querySelector(".blue");
